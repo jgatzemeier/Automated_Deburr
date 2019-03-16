6bf2d5b58573405fae9b74fc6358305235979a01
jgatzemeier <jgatzemeier@email.arizona.edu>
2019-03-16 13:25:27 -0700

    t

--- ActuatorControl.py
+++ ActuatorControl.py
@@ -12,8 +12,8 @@ class ActuatorControl:
     strokeLength = 0
     strokeTime = 0
     speed = 1
-
-    GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)  #pin for relay 1
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)  #pin for relay 1
     GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)  #pin for relay 2
 
     def __init__(self):
@@ -32,12 +32,12 @@ class ActuatorControl:
 
 
     def goUp(self):
-        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(11, GPIO.HIGH)
         GPIO.output(13, GPIO.HIGH)
         time.sleep(type(self).strokeTime)
 
     def goDown(self):
-        GPIO.output(17, GPIO.LOW)
+        GPIO.output(11, GPIO.LOW)
         GPIO.output(13, GPIO.LOW)
         time.sleep(type(self).strokeTime)
 
