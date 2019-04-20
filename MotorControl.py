35bd389aab4ac4513e1cdf38beab8f7bbd8f4ea0
jgatzemeier <jgatzemeier@email.arizona.edu>
2019-04-20 15:33:49 -0700

    pls so help me God work

--- MotorControl.py
+++ MotorControl.py
@@ -15,16 +15,16 @@ class MotorControl:
 
 
     # I2C channel 1 is connected to the GPIO pins
-    # channel = 1
+    channel = 1
     #
     # #  MCP4725 defaults to address 0x60
-    # address = 0x60
+    address = 0x61
     #
     # # Register addresses (with "normal mode" power-down bits)
-    # reg_write_dac = 0x40
+    reg_write_dac = 0x40
     #
     # # Initialize I2C (SMBus)
-    # bus = smbus.SMBus(1)
+    bus = smbus.SMBus(1)
 
     GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) # pin for SPDT switch
 
@@ -39,17 +39,17 @@ class MotorControl:
             #     type(self).dac.set_voltage(x)
             #     time.sleep(2)
         # type(self).dac.set_voltage(type(self).voltage)
-            # Create a sawtooth wave 16 times
-            # for i in range(0x10000):
-            #     # Create our 12-bit number representing relative voltage
-            #     voltage = i & 0xfff
-            #
-            #     # Shift everything left by 4 bits and separate bytes
-            #     msg = (voltage & 0xff0) >> 4
-            #     msg = [msg, (msg & 0xf) << 4]
-            #
-            #     # Write out I2C command: address, reg_write_dac, msg[0], msg[1]
-            #     type(self).bus.write_i2c_block_data(type(self).address, type(self).reg_write_dac, msg)
+            Create a sawtooth wave 16 times
+            for i in range(0x10000):
+                # Create our 12-bit number representing relative voltage
+                voltage = i & 0xfff
+
+                # Shift everything left by 4 bits and separate bytes
+                msg = (voltage & 0xff0) >> 4
+                msg = [msg, (msg & 0xf) << 4]
+
+                # Write out I2C command: address, reg_write_dac, msg[0], msg[1]
+                type(self).bus.write_i2c_block_data(type(self).address, type(self).reg_write_dac, msg)
 
 
     def calculateVoltage(self):
